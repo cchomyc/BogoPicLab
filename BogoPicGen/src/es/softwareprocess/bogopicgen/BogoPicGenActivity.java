870c2ed1ceef13afa8f7849f869ba94c731a35c0
cchomyc <cchomyc@ualberta.ca>
2015-02-04 15:34:08 -0700

    Lab5 exercise.

--- BogoPicGen/src/es/softwareprocess/bogopicgen/BogoPicGenActivity.java
+++ BogoPicGen/src/es/softwareprocess/bogopicgen/BogoPicGenActivity.java
@@ -36,6 +36,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import es.softwareprocess.bogopicgen.R.id;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -90,16 +92,16 @@ public class BogoPicGenActivity extends Activity {
 
 	private void setBogoPic() {
 		// TODO: Show a toast with message "Generating Photo"
-		
+		Toast.makeText(this, "Generating photo...", Toast.LENGTH_LONG).show();
 		
 		// TODO: Get a reference to the image button
-		
+		ImageButton ib = (ImageButton)findViewById(R.id.TakeAPhoto);
 		
 		// Generate a bogopic
 		ourBMP = BogoPicGen.generateBitmap(400, 400);
 		
 		// TODO: Assign the bogopic to the button with setImageBitmap
-		
+		ib.setImageBitmap(ourBMP);
 	}
 
 	// Call this to accept
@@ -112,13 +114,19 @@ public class BogoPicGenActivity extends Activity {
 		try {	
 			if (intent.getExtras() != null) {
 				// TODO: If cancelled, show a toast, set result to RESULT_CANCELED, finish and return 
-				
+				if (cancel) {
+					Toast.makeText(this, "Canceled", Toast.LENGTH_LONG);
+					setResult(RESULT_CANCELED);
+					finish();
+				}
 				
 				// If accepted save the picture
 				File intentPicture = getPicturePath(intent);
 				saveBMP(intentPicture, ourBMP);
 				
 				// TODO: set result to RESULT_OK
+				setResult(RESULT_OK);
+				finish();
 				
 			} else {
 				Toast.makeText(this, "Photo Cancelled: No Reciever?",
